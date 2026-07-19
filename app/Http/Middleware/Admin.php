783a5ac716d1e5dd1fad4299be167313031eb081
Dimas Aditya Ramadhan <dimas.adityaramadhan071104@gmail.com>
2026-07-19 16:27:28 +0700

    third commit

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -4,18 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class Admin
 {
     public function handle(Request $request, Closure $next): Response
     {
-        // Pastikan user sudah login dan role-nya 'admin'
-        if (auth()->check() && auth()->user()->role === 'admin') {
+        if (Auth::check() && Auth::user()->role === 'admin') {
             return $next($request);
         }
 
-        // Kalau bukan admin (atau belum login), tolak dengan kode 403 Forbidden
         return response()->json([
             'message' => 'Akses ditolak. Anda bukan Admin Kaparak!'
         ], 403);
